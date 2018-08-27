f941967af88511df4ed10a780c2267b12c223fa9
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-08-27 11:10:27 +0200

    records lezen

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -2,6 +2,7 @@ package be.vdab;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,13 +10,17 @@ public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String UPDATE_PRIJS = "update planten set verkoopprijs = verkoopprijs * 1.1";
+    private static final String SELECT = "select id, naam from leveranciers order by id";
     
     public static void main(String[] args) {
         try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD);
-            Statement statement = connection.createStatement()){
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(SELECT)){
             //System.out.println("Connectie geopend");
-            System.out.println(statement.executeUpdate(UPDATE_PRIJS));
+            //System.out.println(statement.executeUpdate(UPDATE_PRIJS));
+            while(resultSet.next()){
+                System.out.println(resultSet.getInt(1) + " " + resultSet.getString(2));
+            }
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
