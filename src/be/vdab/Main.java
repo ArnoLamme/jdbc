1c8cc2f0bcf1cf4aa12801707cc4d0d7cf5d0bca
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-08-27 11:06:24 +0200

    records wijzigen

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -3,15 +3,19 @@ package be.vdab;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
-    private static final String URL = "jdbc:mysql://localhost/tuincentrum/useSSL=false";
-    private static final String USER = "root";
-    private static final String PASSWORD = "vdab";
+    private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
+    private static final String USER = "cursist";
+    private static final String PASSWORD = "cursist";
+    private static final String UPDATE_PRIJS = "update planten set verkoopprijs = verkoopprijs * 1.1";
     
     public static void main(String[] args) {
-        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD)){
-            System.out.println("Connectie geopend");
+        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD);
+            Statement statement = connection.createStatement()){
+            //System.out.println("Connectie geopend");
+            System.out.println(statement.executeUpdate(UPDATE_PRIJS));
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
