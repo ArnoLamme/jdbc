322bc3d0058b50f443465284806505a9216d2b1b
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-08-28 10:44:13 +0200

    driver metadata

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -1,31 +1,22 @@
 package be.vdab;
 
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String SELECT = "select naam from leveranciers where woonplaats = ?";
+//    private static final String SELECT = "select id, voornaam, indienst from werknemers";
     
     public static void main(String[] args) {
-        System.out.println("Woonplaats:");
-        String woonplaats = new Scanner(System.in).nextLine();
-        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD);
-            PreparedStatement statement = connection.prepareStatement(SELECT)){
+        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD)){
             //System.out.println("Connectie geopend");
             //System.out.println(statement.executeUpdate(UPDATE_PRIJS));
-            statement.setString(1, woonplaats);
-            try(ResultSet resultSet = statement.executeQuery()){
-                while(resultSet.next()){
-                    System.out.println(resultSet.getString("naam"));
-                }
-            }
+            DatabaseMetaData metaData = connection.getMetaData();
+            System.out.println(metaData.getDriverName() + " " + metaData.getDriverMajorVersion() + " " + metaData.getDriverMinorVersion());
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
