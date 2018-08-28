3901c85e646a117038d8c59f224146d2ae29e30a
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-08-28 10:52:21 +0200

    resultset metadata

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -1,20 +1,26 @@
 package be.vdab;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-//    private static final String SELECT = "select id, voornaam, indienst from werknemers";
+    private static final String SELECT = "select id, voornaam, indienst from werknemers";
     
     public static void main(String[] args) {
-        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD)){
-            DatabaseMetaData metaData = connection.getMetaData();
-            System.out.println(metaData.getDatabaseProductName() + " " + metaData.getDatabaseMajorVersion() + " " + metaData.getDatabaseMinorVersion());
+        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery(SELECT)){
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            for(int index = 1; index <= metaData.getColumnCount(); index++){
+                System.out.println(metaData.getColumnName(index) + " " + metaData.getColumnTypeName(index));
+            }
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
