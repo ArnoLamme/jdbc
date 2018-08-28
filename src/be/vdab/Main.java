2249e1f43dbfb08a34969ef1f9213069cd448542
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-08-28 16:01:49 +0200

    serializable isolation level

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -2,27 +2,37 @@ package be.vdab;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String UPDATE_PRIJS_10_PROCENT = "update planten "
-            + "set verkoopprijs = verkoopprijs * 1.1 "
-            + "where verkoopprijs >= 100";
-    private static final String UPDATE_PRIJS_5_PROCENT = "update planten "
-            + "set verkoopprijs = verkoopprijs * 1.05 "
-            + "where verkoopprijs < 100";
+    private static final String SELECT = "select id from soorten where naam = ?";
+    private static final String INSERT = "insert into soorten(naam) values (?)";
     
     public static void main(String[] args) {
+        System.out.println("Soortnaam:");
+        String soortNaam = new Scanner(System.in).nextLine();
         try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                Statement statement = connection.createStatement()){
+                PreparedStatement statementSelect = connection.prepareStatement(SELECT);
+                PreparedStatement statementInsert = connection.prepareStatement(INSERT)){
+            statementSelect.setString(1, soortNaam);
+            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
             connection.setAutoCommit(false);
-            statement.executeUpdate(UPDATE_PRIJS_10_PROCENT);
-            statement.executeUpdate(UPDATE_PRIJS_5_PROCENT);
-            connection.commit();
+            try(ResultSet resultSet = statementSelect.executeQuery()){
+                if(resultSet.next()){
+                    System.out.println("Soort met deze naam bestaat al");
+                }
+                else{
+                    statementInsert.setString(1, soortNaam);
+                    statementInsert.executeUpdate();
+                    connection.commit();
+                }
+            }
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
