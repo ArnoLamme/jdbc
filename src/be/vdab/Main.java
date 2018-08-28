c5b3a1472455b26af09aa027f676dec62acac026
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-08-28 13:57:27 +0200

    no autocommit

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -1,29 +1,28 @@
 package be.vdab;
 
-import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.Statement;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String CALL = "{call PlantenMetEenWoord(?)}";
+    private static final String UPDATE_PRIJS_10_PROCENT = "update planten "
+            + "set verkoopprijs = verkoopprijs * 1.1 "
+            + "where verkoopprijs >= 100";
+    private static final String UPDATE_PRIJS_5_PROCENT = "update planten "
+            + "set verkoopprijs = verkoopprijs * 1.05 "
+            + "where verkoopprijs < 100";
     
     public static void main(String[] args) {
-        System.out.println("Woord:");
-        String woord = new Scanner(System.in).nextLine();
         try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                CallableStatement statement = connection.prepareCall(CALL)){
-            statement.setString(1, "%" + woord + "%");
-            try(ResultSet resultSet = statement.executeQuery()){
-                while(resultSet.next()){
-                    System.out.println(resultSet.getString("naam"));
-                }
-            }
+                Statement statement = connection.createStatement()){
+            connection.setAutoCommit(false);
+            statement.executeUpdate(UPDATE_PRIJS_10_PROCENT);
+            statement.executeUpdate(UPDATE_PRIJS_5_PROCENT);
+            connection.commit();
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
