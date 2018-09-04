6c4f0e293519c6833dc34c25414da792729c7ab6
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-09-04 09:48:09 +0200

    batch updates

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -2,37 +2,31 @@ package be.vdab;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Scanner;
+import java.sql.Statement;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String SELECT = "select id from soorten where naam = ?";
-    private static final String INSERT = "insert into soorten(naam) values (?)";
+    private static final String UPDATE_PRIJS_10_PROCENT = "update planten "
+            + "set verkoopprijs = verkoopprijs * 1.1 "
+            + "where verkoopprijs >= 100";
+    private static final String UPDATE_PRIJS_5_PROCENT = "update planten "
+            + "set verkoopprijs = verkoopprijs * 1.05 "
+            + "where verkoopprijs < 100";
     
     public static void main(String[] args) {
-        System.out.println("Soortnaam:");
-        String soortNaam = new Scanner(System.in).nextLine();
-        try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                PreparedStatement statementSelect = connection.prepareStatement(SELECT);
-                PreparedStatement statementInsert = connection.prepareStatement(INSERT)){
-            statementSelect.setString(1, soortNaam);
-            connection.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
+        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement()){
+            statement.addBatch(UPDATE_PRIJS_10_PROCENT);
+            statement.addBatch(UPDATE_PRIJS_5_PROCENT);
+            connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             connection.setAutoCommit(false);
-            try(ResultSet resultSet = statementSelect.executeQuery()){
-                if(resultSet.next()){
-                    System.out.println("Soort met deze naam bestaat al");
-                }
-                else{
-                    statementInsert.setString(1, soortNaam);
-                    statementInsert.executeUpdate();
-                    connection.commit();
-                }
-            }
+            int[] aantalGewijzigdeRecordsPerUpdate = statement.executeBatch();
+            System.out.println(aantalGewijzigdeRecordsPerUpdate[0] + " planten met 10% verhoogd");
+            System.out.println(aantalGewijzigdeRecordsPerUpdate[1] + " planten met 5% verhoogd");
+            connection.commit();
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
