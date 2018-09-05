c056629a0b214fa8571737ce304a1ba82ecab593
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-09-05 11:12:06 +0200

    datum en tijd functies

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -2,34 +2,27 @@ package be.vdab;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.util.Scanner;
+import java.sql.Statement;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String SELECT = "select indienst, voornaam, familienaam "
+    private static final String SELECT = "select geboorte, voornaam, familienaam "
             + "from werknemers "
-            + "where indienst >= ? "
-            + "order by indienst";
+            + "where {fn month(geboorte)} = {fn month({fn curdate()})} "
+            + "order by {fn dayofmonth(geboorte)}";
     
     public static void main(String[] args) {
-        System.out.println("Datum vanaf (dd/mm/yyyy):");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/y");
-        LocalDate datum = LocalDate.parse(new Scanner(System.in).nextLine(), formatter);
-        try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                PreparedStatement statement = connection.prepareStatement(SELECT)){
+        try(Connection connection  = DriverManager.getConnection(URL, USER, PASSWORD);
+                Statement statement = connection.createStatement()){
             connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-            statement.setDate(1, java.sql.Date.valueOf(datum));
             connection.setAutoCommit(false);
-            try(ResultSet resultSet = statement.executeQuery()){
+            try(ResultSet resultSet = statement.executeQuery(SELECT)){
                 while(resultSet.next()){
-                    System.out.println(resultSet.getDate("indienst") + " " + resultSet.getString("voornaam") + " " + resultSet.getString("familienaam"));
+                    System.out.println(resultSet.getDate("geboorte") + " " + resultSet.getString("voornaam") + " " + resultSet.getString("familienaam"));
                 }
             }
             connection.commit();
