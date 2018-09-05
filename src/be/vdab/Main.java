935606d0331dd50a37b2ee18ee378f736256c47c
Arno Lambrechts <arno_lambrechts@hotmail.com>
2018-09-05 09:49:09 +0200

    datum en tijd

--- src/be/vdab/Main.java
+++ src/be/vdab/Main.java
@@ -2,32 +2,30 @@ package be.vdab;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.Scanner;
 
 public class Main {
     private static final String URL = "jdbc:mysql://localhost/tuincentrum?useSSL=false";
     private static final String USER = "cursist";
     private static final String PASSWORD = "cursist";
-    private static final String INSERT = "insert into soorten(naam) values(?)";
+    private static final String SELECT = "select indienst, voornaam, familienaam "
+            + "from werknemers "
+            + "where indienst >= {d '2001-1-1'} "
+            + "order by indienst";
     
     public static void main(String[] args) {
-        System.out.println("Naam:");
-        String naam = new Scanner(System.in).nextLine();
         try(Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
-                PreparedStatement statement = connection.prepareStatement(INSERT, Statement.RETURN_GENERATED_KEYS)){
+                Statement statement = connection.createStatement()){
             connection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-            statement.setString(1, naam);
             connection.setAutoCommit(false);
-            statement.executeUpdate();
-            try(ResultSet resultSet = statement.getGeneratedKeys()){
-                resultSet.next();
-                System.out.println(resultSet.getLong(1));
-                connection.commit();
+            try(ResultSet resultSet = statement.executeQuery(SELECT)){
+                while(resultSet.next()){
+                    System.out.println(resultSet.getDate("indienst") + " " + resultSet.getString("voornaam") + " " + resultSet.getString("familienaam"));
+                }
             }
+            connection.commit();
         }
         catch(SQLException ex){
             ex.printStackTrace(System.err);
